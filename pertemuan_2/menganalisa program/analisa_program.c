1389c16681ac2493ca8f1ea2537be1a1e76d7829
Muhammad Fierlyan Irwandi <67852363+feboyfierlyan@users.noreply.github.com>
2024-09-03 10:50:15 +0700

    Add files via upload
    up tugas analisa program

---
+++ pertemuan_2/menganalisa program/analisa_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, c, d;
+    double e, f, g, h;
+
+    a=75, b=259, c=3315, d=12345;
+    e=75.07, f=259.055, g=3315.3310, h=12345.67890;
+
+    printf("a=%7d\nb=%7d\nc=%7d\nd=%7d\n\n", a, b, c, d);
+    printf("e=%12.5f\nf=%12.5f\ng=%12.5f\nh=%12.5f\n", e, f, g, h);
+}
