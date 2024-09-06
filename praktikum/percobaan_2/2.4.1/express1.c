6d72d9eed69c4e02ad1301a195462b68a2964347
Muhammad Fierlyan Irwandi <67852363+feboyfierlyan@users.noreply.github.com>
2024-09-06 22:43:35 +0700

    percobaan 2.4.1

---
+++ praktikum/percobaan_2/2.4.1/express1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+        int bulat = 32767;
+        float pecahan = 339.1234567;
+        double ganda = 3.4567890e+11;
+        char karakter = 'W';
+
+        printf("Variabel bilangan bulat = %d\n\n", bulat);
+        printf("Variabel bilangan pecahan = %f\n\n", pecahan);
+        printf("Variabel bilangan pecahan2 = %e\n\n", ganda);
+        printf("Variabel karakter = %c\n\n", karakter);
+
+        getch();
+}
