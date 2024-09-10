1812c4ce98d62ea7e0e0c130a87780763a646630
Muhammad Fierlyan Irwandi <67852363+feboyfierlyan@users.noreply.github.com>
2024-09-10 12:57:40 +0700

    kode 2.4.3

---
+++ praktikum/percobaan_2/2.4.3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int a = 12;
+    int b = 8;
+    int hasil;
+    float c = 11.0;
+    float d = 7.0;
+
+    printf("6 + a / 5 * b = %d\n\n", 6 + a / 5 * b);
+    printf("a / b * b = %d\n\n", a / b * b);
+    printf("c / d * d = %f\n\n", c / d * d);
+    printf("-a = %d\n\n", -a);
+
+}
